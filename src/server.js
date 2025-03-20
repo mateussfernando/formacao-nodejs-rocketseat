34809da919c483c677c09ec64ca3a432d73cf527
Mateus Fernando <mateusfss368@gmail.com>
2025-03-20 18:14:32 +0000

    node --watch

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import http from 'node:http'; //Esmodules => import/export
 // req = request
 // res = response
 const server = http.createServer((req, res) => {
-    return res.end('Hello World');
+    return res.end('Hello World Watch');
 })
 
 server.listen(3333);
