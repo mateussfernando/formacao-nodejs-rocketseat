274606c9d7959b5874fdb888b0ee35dd622538dc
Mateus Fernando <mateusfss368@gmail.com>
2025-03-25 01:29:25 -0300

    2.1 Rotas de criação e listagem (Métodos HTTP)

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
 // const http = require('http'); CommonJS => require
 import http from 'node:http'; //Esmodules => import/export
 
-// req = request
-// res = response
+// Uma requisição HTTP é composta por dois principais recursos: 
+
+// Método HTTP
+// URL
+
+// Métodos HTTPS: GET, POST, PUT, PATCH, DELETE
+
+// GET = Buscar uma informação do back-end
+// POST = Criar Uma informação no back-end
+// PUT = Atualizar um recurso no back-end
+// PATCH = Atualizar uma informação específica de um recurso no back-end
+// DELETE = Deletar um recurso do back-end
+
+// GET /users = Buscando usuários no back-end
+// POST /users = Criar um usuário no back-end
+
 const server = http.createServer((req, res) => {
+    const {method, url} = req;
+
+    if (method == 'GET' && url == '/users') {
+        return res.end('Listagem de usuários');
+    }
+    if (method == "POST" && url == "/users") {
+      return res.end("Criação de usuários");
+    }
+    
+
     return res.end('Hello World Watch');
 })
 
